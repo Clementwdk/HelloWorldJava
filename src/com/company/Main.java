50afcc009fa24cf3350d79e3f9df170835263d0f
Clementwdk <wodkowskiclement@orange.fr>
2020-12-05 16:05:09 +0000

    fix class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Nom firstcharact =new Nom() ;
+        Nom firstcharact =new Nom();
         System.out.println("HELLO  !!!!!"+ firstcharact.FName+" "+ firstcharact.SName  );
     }
 }
